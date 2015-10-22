0bcb4efbc3105cc77ddf0f418eade76e9abdae31
Sævar Öfjörð Magnússon <saevar@overcast.io>
2015-10-22 13:52:45 +0000

    Debugh

--- oc_core/middleware.py
+++ oc_core/middleware.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect
 
 class Redirect404Middleware(object):
     def process_response(self, request, response):
+        print response
         if response == Http404:
             return redirect('/404/')
 
