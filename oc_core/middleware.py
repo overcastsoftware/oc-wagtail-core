e3d1797c77f098acba8d546691d505482797c8e6
Sævar Öfjörð Magnússon <saevar@overcast.io>
2015-10-22 13:58:08 +0000

    Use status code

--- oc_core/middleware.py
+++ oc_core/middleware.py
@@ -1,11 +1,9 @@
-from django.http import Http404
 from django.shortcuts import redirect
 
 
 class Redirect404Middleware(object):
     def process_response(self, request, response):
-        print response
-        if response == Http404:
+        if response.status_code != 404:
             return redirect('/404/')
 
         return response
