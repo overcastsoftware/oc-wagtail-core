816ea9d55807d7fcf493c92661886676b6abbd61
Sævar Öfjörð Magnússon <saevar@overcast.io>
2015-10-22 14:06:28 +0000

    Add 500 error

--- oc_core/middleware.py
+++ oc_core/middleware.py
@@ -3,7 +3,9 @@ from django.shortcuts import redirect
 
 class Redirect404Middleware(object):
     def process_response(self, request, response):
-        if response.status_code != 404:
+        if response.status_code == 404:
             return redirect('/404/')
+        elif response.status_code == 500:
+            return redirect('/500/')
 
         return response
