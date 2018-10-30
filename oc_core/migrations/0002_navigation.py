f84a2edf484f99bde5d1afb61f1e513d63faa3f7
Eirikur Ingi Magnusson <eiiki0808@gmail.com>
2018-10-30 16:23:38 +0000

    Update 0002_navigation.py

--- oc_core/migrations/0002_navigation.py
+++ oc_core/migrations/0002_navigation.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import wagtail.core.fields
-import wagtail.core.blocks
+import wagtail
+if wagtail.VERSION[0] >= 2:
+    import wagtail.core.fields as fields
+    import wagtail.core.blocks as blocks
+else:
+    import wagtail.wagtailcore.fields as fields
+    import wagtail.wagtailcore.blocks as blocks
 
 
 class Migration(migrations.Migration):
@@ -19,7 +24,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=255)),
                 ('slug', models.SlugField(max_length=255)),
-                ('navigation', wagtail.core.fields.StreamField([(b'menu_block', wagtail.core.blocks.StructBlock([(b'title', wagtail.core.blocks.CharBlock()), (b'menu_items', wagtail.core.blocks.ListBlock(wagtail.core.blocks.StreamBlock([(b'link_external', wagtail.core.blocks.StructBlock([(b'caption', wagtail.core.blocks.CharBlock()), (b'url', wagtail.core.blocks.CharBlock())])), (b'link_page', wagtail.core.blocks.PageChooserBlock())])))]))], blank=True)),
+                ('navigation', fields.StreamField([(b'menu_block', blocks.StructBlock([(b'title', blocks.CharBlock()), (b'menu_items', blocks.ListBlock(blocks.StreamBlock([(b'link_external', blocks.StructBlock([(b'caption', blocks.CharBlock()), (b'url', blocks.CharBlock())])), (b'link_page', blocks.PageChooserBlock())])))]))], blank=True)),
             ],
         ),
     ]
